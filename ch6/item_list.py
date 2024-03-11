f633779e93ca2ff5eef752074c7a71ef7d42c9f7
BynJerL <yggcphanq@gmail.com>
2024-03-11 23:01:21 +0700

    fix file

--- ch6/item_list.py
+++ ch6/item_list.py
@@ -68,30 +68,3 @@ class ItemApp:
     def delete_item(self):
         # Will be added soon
         pass
-
-def main():
-    app = ItemApp()
-    while True:
-        print("\nItem List App:")
-        print("1. Add Item")
-        print("2. View Item")
-        print("3. Exit")
-        choice = input("Enter your choice: ")
-        if choice == '1':
-            itemName = input("Enter item name: ")
-            itemType = input("Enter item type: ")
-            itemCat = input("Enter item category: ")
-            ammount = input("Enter item ammount: ")
-            app.add_item(itemName, itemType, itemCat, ammount)
-
-        elif choice == '2':
-            app.view_item()
-
-        elif choice == '3':
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
