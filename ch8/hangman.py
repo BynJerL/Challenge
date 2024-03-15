1ed83990d237a664cb096f2d6eb6ba1e3539829f
BynJerL <yggcphanq@gmail.com>
2024-03-15 20:37:41 +0700

    Challenge 8: Hangman

---
+++ ch8/hangman.py
@@ -0,0 +1,20 @@
+from tabulate import tabulate
+
+# Hangman States
+Hangman0 : str = "\n\n\n\n\n"
+Hangman1 : str = "\n\n\n\n\n _____"
+Hangman2 : str = "|\n|\n|\n|\n|\n|_____"
+Hangman3 : str = "|____\n|\n|\n|\n|\n|_____"
+Hangman4 : str = "|____\n|   |\n|\n|\n|\n|_____"
+Hangman5 : str = "|____\n|   |\n|   O\n|\n|\n|_____"
+Hangman6 : str = "|____\n|   |\n|   O\n|   |\n|\n|_____"
+Hangman7 : str = "|____\n|   |\n|   O\n|  /|\n|\n|_____"
+Hangman8 : str = "|____\n|   |\n|   O\n|  /|\\\n|\n|_____"
+Hangman9 : str = "|____\n|   |\n|   O\n|  /|\\\n|  /\n|_____"
+Hangman10 : str = "|____\n|   |\n|   O\n|  /|\\\n|  / \\\n|______"
+
+HangmanStates : list = [Hangman0, Hangman1, Hangman2, Hangman3, Hangman4, Hangman5, Hangman6, Hangman7, Hangman8, Hangman9, Hangman10]
+
+for state in HangmanStates:
+    print(f"State {HangmanStates.index(state)}:")
+    print(state)
